85b3787d2101754fd46a8435a9dd7c6582e2d44c
Louis <mayco.louis@gmail.com>
2022-03-05 01:19:56 +0700

    try open website and get page HTML

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -1,4 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+DRIVER_PATH = r'C:\Users\fxnic\Documents\ChromeDriver\chromedriver.exe'
 
 options = Options()
+options.headless = True
+options.add_argument("--window-size=1920,1200")
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+driver.get("https://nswi.bkpm.go.id/data_statistik")
+
+print(driver.page_source)
+# driver.quit()
