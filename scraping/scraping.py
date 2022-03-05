25105df5dbdff5660109a70ffd9ec54ef0c9d5a5
Louis <mayco.louis@gmail.com>
2022-03-05 21:17:18 +0700

    add customizable input field for scraping

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -3,18 +3,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by  import By
 
-def open_url(url):
+def check_params(params):
+    for key, value in params.items():
+        print("{}\t:{}".format(key,value))
+
+def scrape(params):
+    """use to run the scraping script"""
+    url = params['url']
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
     driver.get(url)
 
-    print(driver.page_source)
-    insert_fields(driver)
+    # insert_fields(driver)
     # driver.quit()
     return 0
 
 def insert_fields(driver):
     negara = driver.find_element(By.CLASS_NAME('col-md-8 col-sm-12"')).click()
-
-if __name__ == '__main__':
-    open_url("https://nswi.bkpm.go.id/data_statistik")
--- script/main.py
+++ script/main.py
@@ -1,7 +1,32 @@
+"""
+Run This script only
+Change main function according to the needs,
+use params to modified the result from the scraping
+"""
+
+from scraping.scraping import scrape
+from scraping.scraping import check_params
 
 
 def main():
+    params = {
+        'url': "https://nswi.bkpm.go.id/data_statistik",
+        'field': 'Negara',
+        'periode': 'Per Tahun',
+        'tahunAwal': 2017,
+        'tahunAkhir': 2018,
+        'negara': 'Indonesia',
+        'provinsi': 'Daerah Khusus Ibukota Jakarta',
+        'kabupaten': None,
+        'sektor': None,
+        'kbli': None
+    }
+    # uncomment for checking passing parameter
+    # check_params(params)
+    scrape(params)
+
+    return 0
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
