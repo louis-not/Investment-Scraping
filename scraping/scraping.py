4fd5d5f164b8d07e922543aa8133f2f039691ee6
Louis <mayco.louis@gmail.com>
2022-03-06 16:06:07 +0700

    minor fix on scraping

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -13,14 +13,16 @@ def scrape(params):
     url = params['url']
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(url)
-    time.sleep(3)
+    time.sleep(2)
     try:
         insertion = insert_fields(driver, params)
+        time.sleep(4)
+        scraping = get_data(driver)
 
-        if insertion:
+        if insertion and scraping :
             print('process succeed')
     finally:
-        time.sleep(5)
+        time.sleep(10)
         driver.quit()
 
     return 0
@@ -36,6 +38,22 @@ def insert_fields(driver,params):
     driver.find_element(By.XPATH, '//*[@id="tahunAkhir_chosen"]/div/div/input').send_keys(str(params['tahunAkhir']) + '\n')
     driver.find_element(By.XPATH, '//*[@id="filterNegara_chosen"]').click()
     driver.find_element(By.XPATH, '//*[@id="filterNegara_chosen"]/ul/li/input').send_keys(params['negara'] + '\n')
+    driver.find_element(By.XPATH, '//*[@id="filterProv_chosen"]').click()
+    driver.find_element(By.XPATH, '//*[@id="filterProv_chosen"]/ul/li/input').send_keys(params['provinsi'] + '\n')
     driver.find_element(By.XPATH, '//*[@id="peringkat"]/table/tbody/tr[4]/td/div/button').click()
 
     return True
+
+def get_data(driver):
+    """scrape data from intendeed page"""
+    text = driver.find_element(By.XPATH,
+                               '/html/body/form[1]/table/tbody/tr[2]/td/div/div[1]/table/tbody/tr[2]/td/table[2]/tbody/tr[2]/td[3]/div/span').text
+    if text == 'No Data Available':
+        print('No data')
+    # try :
+    #
+    #     pass
+    # except:
+    #
+    # finally:
+        return True
